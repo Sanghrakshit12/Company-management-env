9f9de961494c310db6ee0dc72a9f81f50d53ba2b
samendra23 <117721558+samendra23@users.noreply.github.com>
2022-11-12 18:46:46 +0530

    main file structure
    only contains menu

---
+++ main1.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <fstream>
+#include <conio.h>
+#include <stdlib.h>
+#include <iomanip> 
+using namespace std;
+
+int main(){
+    int choice;
+    cout << endl << setw(75) << "Welcome to the TATA Group" << endl;
+    cout << "\nWhich page would you like to visit:\n1. TATA Motors\n2. TITAN\n3. Croma Digital Store\nYour choice: ";
+    cin >> choice;
+    switch(choice){
+        case 1:
+        //motors();
+        break;
+        case 2:
+        //watches();
+        break;
+        case 3:
+        //croma();
+        break;
+    }
+    return 0;
+}
